7c0d18ff85de74e462fbbfeebcd4d57addc8e0aa
Márcio Lopes Fão <marcio.lopes.fao@gmail.com>
2024-01-21 23:24:03 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,5 +6,52 @@ $url='https://api.cittamobi.com.br/m3p/js/prediction/stop/5443184?fbclid=IwAR2V-
 $json = file_get_contents($url);
 $dados=json_decode($json);
 
-//var_dump($dados->services[0]->routeMnemonic);
-die($dados->services[0]->routeMnemonic);
+//var_dump($dados->services[0]); die($dados->services[0]->routeMnemonic);
+
+if(isset($_GET['c'])){
+    detalha_servico($_GET['c'], $dados);
+    die;
+}
+?>
+
+<h1>Selecione um Serviço</h1>
+<ul>
+<?php foreach ($dados->services as $service):?>
+    <li><a href="?c=<?php echo( $service->serviceId) ?>"><?php echo $service->serviceMnemonic ?></a></li>
+<?php endforeach ?>
+</ul> 
+
+<?php 
+
+function detalha_servico($codigo, $dados=null){
+    ?>
+ <a href="javascript:history.back()">< voltar</a>
+    <h1>Detalhes do Serviço</h1>
+    <ul>
+        <?php foreach ($dados->services as $key=>$service):?>
+            <?php if($service->serviceId==$codigo): ?>
+                <?php // var_dump((array) $service);die; ?>
+                <?php foreach ( (array) $service as $k=>$item): ?>
+                    <?php if(is_array($item)): ?>
+                        <?php  //var_dump($item);die; ?>
+                        <li><?php echo $k ?>:</li>
+                        <?php if(sizeof($item)>0): ?>
+                            <ul>
+                                <?php foreach ($item as $v): ?>
+                                    <li><?php echo $v ?> </li>
+                                <?php endforeach ?>
+                            </ul>
+                        <?php endif ?>
+                    <?php else: ?>
+                        <li><?php echo $k ?>: <?php echo $item ?> </li>
+                    <?php endif ?>
+                <?php endforeach ?>
+            <?php endif ?>
+        <?php endforeach ?>
+    </ul>
+    <a href="javascript:history.back()  ">< voltar</a>
+
+    <?php
+}
+
+?>
